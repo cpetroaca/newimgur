5a296a406a1c299ee23149ac188aa2f6b2c9f5ec
Cristian Petroaca <cristian.petroaca@gmail.com>
2022-11-22 22:57:52 +0200

    Added Spring Boot skeleton

---
+++ image/src/main/java/com/newimgur/image/service/ImageService.java
@@ -0,0 +1,14 @@
+package com.newimgur.image.service;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+import com.newimgur.image.model.Image;
+
+@Service
+public class ImageService {
+   public Image getImage() {
+        UUID uuid = UUID.randomUUID();
+        return new Image(uuid, ZonedDateTime.now(), "Hello");
+   } 
+}
