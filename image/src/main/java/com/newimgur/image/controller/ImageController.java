8e1dddc79558cef003052b30e5905258c3e7f943
Cristian Petroaca <cristian.petroaca@gmail.com>
2022-12-14 13:09:06 +0200

    Added logging.

--- image/src/main/java/com/newimgur/image/controller/ImageController.java
+++ image/src/main/java/com/newimgur/image/controller/ImageController.java
@@ -1,5 +1,7 @@
 package com.newimgur.image.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,13 +17,15 @@ import com.newimgur.image.service.ImageService;
 
 @RestController
 public class ImageController {
-
+	private final Logger LOGGER = LoggerFactory.getLogger(ImageController.class);
+	
 	@Autowired
 	ImageService imageService;
 
 	@GetMapping("/images/{id}")
 	public Image getImage(@PathVariable String id) {
 		try {
+			LOGGER.info("Requested image with id {}", id);
 			return imageService.getImage(id);
 		} catch (IllegalArgumentException iae) {
 			throw new ResponseStatusException(
@@ -32,6 +36,7 @@ public class ImageController {
 	@PostMapping(value = "/images" , consumes = { MediaType.APPLICATION_JSON_VALUE,
 												  MediaType.MULTIPART_FORM_DATA_VALUE})
 	public Image createImage(@RequestPart String caption) {
+		LOGGER.info("Created image with caption {}", caption);
 		return imageService.createImage(caption);
 	}
 }
