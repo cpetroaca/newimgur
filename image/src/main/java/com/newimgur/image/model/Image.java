ec5f044b4c81e2e138d483a19e35a1958f5701fd
Cristian Petroaca <cristian.petroaca@gmail.com>
2022-12-16 21:48:31 +0200

    Added GCP Spanner interface

--- image/src/main/java/com/newimgur/image/model/Image.java
+++ image/src/main/java/com/newimgur/image/model/Image.java
@@ -1,24 +1,28 @@
 package com.newimgur.image.model;
 
-import java.time.ZonedDateTime;
-import java.util.UUID;
+import org.springframework.cloud.gcp.data.spanner.core.mapping.PrimaryKey;
+import org.springframework.cloud.gcp.data.spanner.core.mapping.Table;
 
+import com.google.cloud.Timestamp;
+
+@Table(name="Images")
 public class Image {
-    private UUID id;
-    private ZonedDateTime createdAt;
+	@PrimaryKey
+    private String id;
+    private Timestamp createdAt;
     private String caption;
 
-    public Image(UUID id, ZonedDateTime createdAt, String caption) {
+    public Image(String id, Timestamp createdAt, String caption) {
         this.id = id;
         this.createdAt = createdAt;
         this.caption = caption;
     }
 
-    public UUID getId() {
+    public String getId() {
         return this.id;
     }
 
-    public ZonedDateTime getCreatedAt() {
+    public Timestamp getCreatedAt() {
         return this.createdAt;
     }
 
---
+++ image/src/main/java/com/newimgur/image/repository/ImagesRepository.java
@@ -0,0 +1,10 @@
+package com.newimgur.image.repository;
+
+import org.springframework.cloud.gcp.data.spanner.repository.SpannerRepository;
+import org.springframework.stereotype.Repository;
+
+import com.newimgur.image.model.Image;
+
+@Repository
+public interface ImagesRepository extends SpannerRepository<Image, String> {
+}
